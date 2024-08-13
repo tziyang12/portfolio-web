2d277f767ad7e5e365b38da3d997e4a33ddf7465
tziyang12 <37449750+tziyang12@users.noreply.github.com>
2024-08-13 11:25:26 +0800

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -34,7 +34,6 @@ function App() {
       <Router >
         <Navbar setDarkMode={setDarkMode}/>
         <Body>
-          <Analytics />
           <HeroSection />
           <Wrapper>
             <Skills />
